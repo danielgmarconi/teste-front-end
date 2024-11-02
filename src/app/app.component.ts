75a6a8e08b9b5b893132460b1873f675179200a3
danielgmarconi <danielgmarconi@gmail.com>
2024-11-02 12:39:31 -0300

    Implementações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import  {  NgxSpinnerModule  }  from  "ngx-spinner" ;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet,
+           HeaderComponent,
+           FooterComponent,
+           NgxSpinnerModule],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'teste-front-end';
